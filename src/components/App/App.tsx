8d99854ecf6d93313ab889d4c4c0fd1e09b3d81c
Palina Karabovich <polina.korobovich@yandex.by>
2023-06-22 15:50:20 +0200

    directories created

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.module.css';
+import FirstPage from '../FirstPage/FirstPage';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <FirstPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstPage/FirstPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './FirstPage.module.css'
+
+const FirstPage: React.FC = () => {
+  return (
+    <section>first page</section>
+  )
+}
+
+export default FirstPage;
