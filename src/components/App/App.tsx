15ebeab1096b3a74927e75657171199c72f78f80
Palina Karabovich <polina.korobovich@yandex.by>
2023-06-22 16:55:20 +0200

    form created, sidebar added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,55 @@ import FirstPage from '../FirstPage/FirstPage';
 function App() {
   return (
     <div className={styles.app}>
-      <FirstPage />
+      <section className={styles.page}>
+        <ul className={styles.sidebar}>
+          <li className={styles.step}>
+            <div className={styles.circle}>
+              <span className={styles.number}>1</span>
+            </div>
+            <div className={styles.step__description}>
+              <p className={styles.step__counter}>step 1</p>
+              <p className={styles.step__text}>
+                Your info
+              </p>
+            </div>
+          </li>
+          <li className={styles.step}>
+            <div className={styles.circle}>
+              <span className={styles.number}>2</span>
+            </div>
+            <div className={styles.step__description}>
+              <p className={styles.step__counter}>step 2</p>
+              <p className={styles.step__text}>
+                Select plan
+              </p>
+            </div>
+          </li>
+          <li className={styles.step}>
+            <div className={styles.circle}>
+              <span className={styles.number}>3</span>
+            </div>
+            <div className={styles.step__description}>
+              <p className={styles.step__counter}>step 3</p>
+              <p className={styles.step__text}>
+                Add-ons
+              </p>
+            </div>
+          </li>
+          <li className={styles.step}>
+            <div className={styles.circle}>
+              <span className={styles.number}>4</span>
+            </div>
+            <div className={styles.step__description}>
+              <p className={styles.step__counter}>step 4</p>
+              <p className={styles.step__text}>
+                Summary
+              </p>
+            </div>
+          </li>
+        </ul>
+        <FirstPage />
+      </section>
     </div>
   );
 }
--- src/components/FirstPage/FirstPage.tsx
+++ src/components/FirstPage/FirstPage.tsx
@@ -3,7 +3,22 @@ import styles from './FirstPage.module.css'
 
 const FirstPage: React.FC = () => {
   return (
-    <section>first page</section>
+      <form className={styles.form}>
+        <div>
+          <h2 className={styles.title}>Personal info</h2>
+          <p className={styles.paragraph}>Please provide your name, email address and phone number.</p>
+        </div>
+        <div>
+          <label className={styles.label}>Name</label>
+          <input className={styles.input}></input>
+          <label className={styles.label}>Email Address</label>
+          <input className={styles.input}></input>
+          <label className={styles.label}>Phone Number</label>
+          <input className={styles.input}></input>
+        </div>
+        <button type='submit' className={styles.button}>Next step</button>
+      </form>
+
   )
 }
 
