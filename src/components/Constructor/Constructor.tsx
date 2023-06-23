e076415bd3f63150caad3643f372129495fa59eb
Palina Karabovich <polina.korobovich@yandex.by>
2023-06-23 12:31:03 +0200

    summary designed

--- src/components/Constructor/Constructor.tsx
+++ src/components/Constructor/Constructor.tsx
@@ -3,6 +3,7 @@ import FirstPage from '../FirstPage/FirstPage';
 import styles from './Constructor.module.css'
 import SecondPage from '../SecondPage/SecondPage';
 import ThirdPage from '../ThirdPage/ThirdPage';
+import Summary from '../Summary/Summary';
 
 const Constructor: React.FC = () => {
   return(
@@ -11,7 +12,7 @@ const Constructor: React.FC = () => {
       <h2 className={styles.title}>Personal info</h2>
       <p className={styles.paragraph}>Please provide your name, email address and phone number.</p>
     </div>
-    <ThirdPage />
+    <Summary />
     <div className={styles.buttons}>
     <button className={styles.button__back}>Go back</button>
     <button className={styles.button__next}>Next step</button>
